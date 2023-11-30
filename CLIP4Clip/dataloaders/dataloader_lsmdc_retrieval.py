48ccac2cd87fa71d769f7b3039e96c63b6e8dcd4
Heesu <hs.kim@pia.space>
2023-11-30 13:58:05 +0900

    Practice (#1)
    * setup env
    
    * use torchrun
    
    * training env setup

--- CLIP4Clip/dataloaders/dataloader_lsmdc_retrieval.py
+++ CLIP4Clip/dataloaders/dataloader_lsmdc_retrieval.py
@@ -10,20 +10,22 @@ import json
 import math
 from dataloaders.rawvideo_util import RawVideoExtractor
 
+
 class LSMDC_DataLoader(Dataset):
     """LSMDC dataset loader."""
+
     def __init__(
-            self,
-            subset,
-            data_path,
-            features_path,
-            tokenizer,
-            max_words=30,
-            feature_framerate=1.0,
-            max_frames=100,
-            image_resolution=224,
-            frame_order=0,
-            slice_framepos=0,
+        self,
+        subset,
+        data_path,
+        features_path,
+        tokenizer,
+        max_words=30,
+        feature_framerate=1.0,
+        max_frames=100,
+        image_resolution=224,
+        frame_order=0,
+        slice_framepos=0,
     ):
         self.data_path = data_path
         self.features_path = features_path
@@ -42,23 +44,37 @@ class LSMDC_DataLoader(Dataset):
         assert self.subset in ["train", "val", "test"]
 
         video_json_path_dict = {}
-        video_json_path_dict["train"] = os.path.join(self.data_path, "LSMDC16_annos_training.csv")
-        video_json_path_dict["val"] = os.path.join(self.data_path, "LSMDC16_annos_val.csv")
-        video_json_path_dict["test"] = os.path.join(self.data_path, "LSMDC16_challenge_1000_publictect.csv")
+        video_json_path_dict["train"] = os.path.join(
+            self.data_path, "LSMDC16_annos_training.csv"
+        )
+        video_json_path_dict["val"] = os.path.join(
+            self.data_path, "LSMDC16_annos_val.csv"
+        )
+        video_json_path_dict["test"] = os.path.join(
+            self.data_path, "LSMDC16_challenge_1000_publictect.csv"
+        )
 
         # <CLIP_ID>\t<START_ALIGNED>\t<END_ALIGNED>\t<START_EXTRACTED>\t<END_EXTRACTED>\t<SENTENCE>
         # <CLIP_ID> is not a unique identifier, i.e. the same <CLIP_ID> can be associated with multiple sentences.
         # However, LSMDC16_challenge_1000_publictect.csv has no repeat instances
         video_id_list = []
         caption_dict = {}
-        with open(video_json_path_dict[self.subset], 'r') as fp:
+        with open(video_json_path_dict[self.subset], "r") as fp:
             for line in fp:
                 line = line.strip()
                 line_split = line.split("\t")
                 assert len(line_split) == 6
-                clip_id, start_aligned, end_aligned, start_extracted, end_extracted, sentence = line_split
+                (
+                    clip_id,
+                    start_aligned,
+                    end_aligned,
+                    start_extracted,
+                    end_extracted,
+                    sentence,
+                ) = line_split
                 caption_dict[len(caption_dict)] = (clip_id, sentence)
-                if clip_id not in video_id_list: video_id_list.append(clip_id)
+                if clip_id not in video_id_list:
+                    video_id_list.append(clip_id)
 
         video_dict = {}
         for root, dub_dir, video_files in os.walk(self.features_path):
@@ -76,11 +92,21 @@ class LSMDC_DataLoader(Dataset):
         for clip_id, sentence in caption_dict.values():
             if clip_id not in self.video_dict:
                 continue
-            self.iter2video_pairs_dict[len(self.iter2video_pairs_dict)] = (clip_id, sentence)
+            self.iter2video_pairs_dict[len(self.iter2video_pairs_dict)] = (
+                clip_id,
+                sentence,
+            )
 
-        self.rawVideoExtractor = RawVideoExtractor(framerate=feature_framerate, size=image_resolution)
-        self.SPECIAL_TOKEN = {"CLS_TOKEN": "<|startoftext|>", "SEP_TOKEN": "<|endoftext|>",
-                              "MASK_TOKEN": "[MASK]", "UNK_TOKEN": "[UNK]", "PAD_TOKEN": "[PAD]"}
+        self.rawVideoExtractor = RawVideoExtractor(
+            framerate=feature_framerate, size=image_resolution
+        )
+        self.SPECIAL_TOKEN = {
+            "CLS_TOKEN": "<|startoftext|>",
+            "SEP_TOKEN": "<|endoftext|>",
+            "MASK_TOKEN": "[MASK]",
+            "UNK_TOKEN": "[UNK]",
+            "PAD_TOKEN": "[PAD]",
+        }
 
     def __len__(self):
         return len(self.iter2video_pairs_dict)
@@ -92,8 +118,8 @@ class LSMDC_DataLoader(Dataset):
     def _get_video_id_single(self, path):
         pseudo_video_id_list = []
         video_id_list = []
-        print('Loading json: {}'.format(path))
-        with open(path, 'r') as f:
+        print("Loading json: {}".format(path))
+        with open(path, "r") as f:
             json_data = json.load(f)
 
         for pseudo_video_id in json_data:
@@ -107,25 +133,29 @@ class LSMDC_DataLoader(Dataset):
 
     def _get_captions_single(self, path):
         pseudo_caption_dict = {}
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             json_data = json.load(f)
 
         for pseudo_video_id, v_ in json_data.items():
             pseudo_caption_dict[pseudo_video_id] = {}
             timestamps = v_["timestamps"]
-            pseudo_caption_dict[pseudo_video_id]["start"] = \
-                np.array([int(math.floor(float(itm[0]))) for itm in timestamps], dtype=object)
-            pseudo_caption_dict[pseudo_video_id]["end"] = \
-                np.array([int(math.ceil(float(itm[1]))) for itm in timestamps], dtype=object)
-            pseudo_caption_dict[pseudo_video_id]["text"] = np.array(v_["sentences"], dtype=object)
+            pseudo_caption_dict[pseudo_video_id]["start"] = np.array(
+                [int(math.floor(float(itm[0]))) for itm in timestamps], dtype=object
+            )
+            pseudo_caption_dict[pseudo_video_id]["end"] = np.array(
+                [int(math.ceil(float(itm[1]))) for itm in timestamps], dtype=object
+            )
+            pseudo_caption_dict[pseudo_video_id]["text"] = np.array(
+                v_["sentences"], dtype=object
+            )
         return pseudo_caption_dict
 
     def _get_text(self, video_id, caption):
         k = 1
         choice_video_ids = [video_id]
-        pairs_text = np.zeros((k, self.max_words), dtype=np.long)
-        pairs_mask = np.zeros((k, self.max_words), dtype=np.long)
-        pairs_segment = np.zeros((k, self.max_words), dtype=np.long)
+        pairs_text = np.zeros((k, self.max_words), dtype=np.longlong)
+        pairs_mask = np.zeros((k, self.max_words), dtype=np.longlong)
+        pairs_segment = np.zeros((k, self.max_words), dtype=np.longlong)
 
         for i, video_id in enumerate(choice_video_ids):
             words = self.tokenizer.tokenize(caption)
@@ -154,45 +184,73 @@ class LSMDC_DataLoader(Dataset):
         return pairs_text, pairs_mask, pairs_segment, choice_video_ids
 
     def _get_rawvideo(self, choice_video_ids):
-        video_mask = np.zeros((len(choice_video_ids), self.max_frames), dtype=np.long)
+        video_mask = np.zeros(
+            (len(choice_video_ids), self.max_frames), dtype=np.longlong
+        )
         max_video_length = [0] * len(choice_video_ids)
 
         # Pair x L x T x 3 x H x W
-        video = np.zeros((len(choice_video_ids), self.max_frames, 1, 3,
-                          self.rawVideoExtractor.size, self.rawVideoExtractor.size), dtype=np.float)
+        video = np.zeros(
+            (
+                len(choice_video_ids),
+                self.max_frames,
+                1,
+                3,
+                self.rawVideoExtractor.size,
+                self.rawVideoExtractor.size,
+            ),
+            dtype=np.float16,
+        )
 
         try:
             for i, video_id in enumerate(choice_video_ids):
                 video_path = self.video_dict[video_id]
 
                 raw_video_data = self.rawVideoExtractor.get_video_data(video_path)
-                raw_video_data = raw_video_data['video']
+                raw_video_data = raw_video_data["video"]
 
                 if len(raw_video_data.shape) > 3:
                     raw_video_data_clip = raw_video_data
                     # L x T x 3 x H x W
-                    raw_video_slice = self.rawVideoExtractor.process_raw_data(raw_video_data_clip)
+                    raw_video_slice = self.rawVideoExtractor.process_raw_data(
+                        raw_video_data_clip
+                    )
                     if self.max_frames < raw_video_slice.shape[0]:
                         if self.slice_framepos == 0:
-                            video_slice = raw_video_slice[:self.max_frames, ...]
+                            video_slice = raw_video_slice[: self.max_frames, ...]
                         elif self.slice_framepos == 1:
-                            video_slice = raw_video_slice[-self.max_frames:, ...]
+                            video_slice = raw_video_slice[-self.max_frames :, ...]
                         else:
-                            sample_indx = np.linspace(0, raw_video_slice.shape[0]-1, num=self.max_frames, dtype=int)
+                            sample_indx = np.linspace(
+                                0,
+                                raw_video_slice.shape[0] - 1,
+                                num=self.max_frames,
+                                dtype=int,
+                            )
                             video_slice = raw_video_slice[sample_indx, ...]
                     else:
                         video_slice = raw_video_slice
 
-                    video_slice = self.rawVideoExtractor.process_frame_order(video_slice, frame_order=self.frame_order)
+                    video_slice = self.rawVideoExtractor.process_frame_order(
+                        video_slice, frame_order=self.frame_order
+                    )
 
                     slice_len = video_slice.shape[0]
-                    max_video_length[i] = max_video_length[i] if max_video_length[i] > slice_len else slice_len
+                    max_video_length[i] = (
+                        max_video_length[i]
+                        if max_video_length[i] > slice_len
+                        else slice_len
+                    )
                     if slice_len < 1:
                         pass
                     else:
                         video[i][:slice_len, ...] = video_slice
                 else:
-                    print("video path: {} error. video id: {}".format(video_path, video_id))
+                    print(
+                        "video path: {} error. video id: {}".format(
+                            video_path, video_id
+                        )
+                    )
         except Exception as excep:
             print("Video ids: {}".format(choice_video_ids))
             raise excep
@@ -203,6 +261,8 @@ class LSMDC_DataLoader(Dataset):
 
     def __getitem__(self, feature_idx):
         clip_id, sentence = self.iter2video_pairs_dict[feature_idx]
-        pairs_text, pairs_mask, pairs_segment, choice_video_ids = self._get_text(clip_id, sentence)
+        pairs_text, pairs_mask, pairs_segment, choice_video_ids = self._get_text(
+            clip_id, sentence
+        )
         video, video_mask = self._get_rawvideo(choice_video_ids)
         return pairs_text, pairs_mask, pairs_segment, video, video_mask
--- CLIP4Clip/dataloaders/rawvideo_util.py
+++ CLIP4Clip/dataloaders/rawvideo_util.py
@@ -1,31 +1,50 @@
 import torch as th
 import numpy as np
 from PIL import Image
+
 # pytorch=1.7.1
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+
 # pip install opencv-python
 import cv2
 
-class RawVideoExtractorCV2():
-    def __init__(self, centercrop=False, size=224, framerate=-1, ):
+
+class RawVideoExtractorCV2:
+    def __init__(
+        self,
+        centercrop=False,
+        size=224,
+        framerate=-1,
+    ):
         self.centercrop = centercrop
         self.size = size
         self.framerate = framerate
         self.transform = self._transform(self.size)
 
     def _transform(self, n_px):
-        return Compose([
-            Resize(n_px, interpolation=Image.BICUBIC),
-            CenterCrop(n_px),
-            lambda image: image.convert("RGB"),
-            ToTensor(),
-            Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
-        ])
+        return Compose(
+            [
+                Resize(n_px, interpolation=Image.BICUBIC),
+                CenterCrop(n_px),
+                lambda image: image.convert("RGB"),
+                ToTensor(),
+                Normalize(
+                    (0.48145466, 0.4578275, 0.40821073),
+                    (0.26862954, 0.26130258, 0.27577711),
+                ),
+            ]
+        )
 
-    def video_to_tensor(self, video_file, preprocess, sample_fp=0, start_time=None, end_time=None):
+    def video_to_tensor(
+        self, video_file, preprocess, sample_fp=0, start_time=None, end_time=None
+    ):
         if start_time is not None or end_time is not None:
-            assert isinstance(start_time, int) and isinstance(end_time, int) \
-                   and start_time > -1 and end_time > start_time
+            assert (
+                isinstance(start_time, int)
+                and isinstance(end_time, int)
+                and start_time > -1
+                and end_time > start_time
+            )
         assert sample_fp > -1
 
         # Samples a frame sample_fp X frames.
@@ -33,11 +52,17 @@ class RawVideoExtractorCV2():
         frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         fps = int(cap.get(cv2.CAP_PROP_FPS))
 
+        if frameCount == 0 or fps == 0:
+            raise ValueError("Video file is corrupted.")
+
         total_duration = (frameCount + fps - 1) // fps
         start_sec, end_sec = 0, total_duration
 
         if start_time is not None:
-            start_sec, end_sec = start_time, end_time if end_time <= total_duration else total_duration
+            start_sec, end_sec = (
+                start_time,
+                end_time if end_time <= total_duration else total_duration,
+            )
             cap.set(cv2.CAP_PROP_POS_FRAMES, int(start_time * fps))
 
         interval = 1
@@ -45,7 +70,8 @@ class RawVideoExtractorCV2():
             interval = fps // sample_fp
         else:
             sample_fp = fps
-        if interval == 0: interval = 1
+        if interval == 0:
+            interval = 1
 
         inds = [ind for ind in np.arange(0, fps, interval)]
         assert len(inds) >= sample_fp
@@ -55,12 +81,14 @@ class RawVideoExtractorCV2():
         images, included = [], []
 
         for sec in np.arange(start_sec, end_sec + 1):
-            if not ret: break
+            if not ret:
+                break
             sec_base = int(sec * fps)
             for ind in inds:
                 cap.set(cv2.CAP_PROP_POS_FRAMES, sec_base + ind)
                 ret, frame = cap.read()
-                if not ret: break
+                if not ret:
+                    break
                 frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 images.append(preprocess(Image.fromarray(frame_rgb).convert("RGB")))
 
@@ -70,15 +98,23 @@ class RawVideoExtractorCV2():
             video_data = th.tensor(np.stack(images))
         else:
             video_data = th.zeros(1)
-        return {'video': video_data}
+        return {"video": video_data}
 
     def get_video_data(self, video_path, start_time=None, end_time=None):
-        image_input = self.video_to_tensor(video_path, self.transform, sample_fp=self.framerate, start_time=start_time, end_time=end_time)
+        image_input = self.video_to_tensor(
+            video_path,
+            self.transform,
+            sample_fp=self.framerate,
+            start_time=start_time,
+            end_time=end_time,
+        )
         return image_input
 
     def process_raw_data(self, raw_video_data):
         tensor_size = raw_video_data.size()
-        tensor = raw_video_data.view(-1, 1, tensor_size[-3], tensor_size[-2], tensor_size[-1])
+        tensor = raw_video_data.view(
+            -1, 1, tensor_size[-3], tensor_size[-2], tensor_size[-1]
+        )
         return tensor
 
     def process_frame_order(self, raw_video_data, frame_order=0):
@@ -94,6 +130,7 @@ class RawVideoExtractorCV2():
             raw_video_data = raw_video_data[random_order, ...]
 
         return raw_video_data
+
 
 # An ordinary video frame extractor based CV2
 RawVideoExtractor = RawVideoExtractorCV2
